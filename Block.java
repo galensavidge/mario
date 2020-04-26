6013b6028ff7f4c79b9598f76478ecf89bbcd618
Galen Savidge <galensavidge@gmail.com>
2020-04-25 22:57:49 -0700

    Added a collider to Block; testing collision checking

--- Block.java
+++ Block.java
@@ -1,15 +1,20 @@
 import java.awt.*;
 
 public class Block extends GameObject{
+    public CircleCollider collider;
 
     public Block(int x, int y) {
         super(0, 1);
 
         this.x = x;
         this.y = y;
+
+        this.collider = new CircleCollider(this, 25);
     }
     public void update() {
-
+        if(collider.checkCollision(0, 0, false)) {
+            System.out.println("Collided with something!");
+        }
     }
 
     public void draw() {
