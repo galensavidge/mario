255bc20a3010e4ba290ab5639e69d988814f7fd6
Galen Savidge <galensavidge@gmail.com>
2020-05-14 23:33:14 -0700

    Uploading Tiled templates and map files

---
+++ levels/mm2-smw-snow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="mm2-smw-snow" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="704" columns="16">
+ <image source="../sprites/mm2-smw-snow.png" trans="93bbec" width="273" height="749"/>
+</tileset>
---
+++ levels/mm2-smw.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="mm2-smw" tilewidth="16" tileheight="24" tilecount="6" columns="100">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <tile id="1">
+  <image width="16" height="16" source="../sprites/hardblock.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../sprites/cloudblock.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../sprites/galoomba-walk-1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="24" source="../sprites/mario-walk-1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../sprites/coin-1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../sprites/movingplatform-2.png"/>
+ </tile>
+</tileset>
